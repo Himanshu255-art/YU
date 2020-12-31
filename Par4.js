ca4de4d9c920b13743ad67c0abee4714779fd6ef
Himanshu255-art <73885430+Himanshu255-art@users.noreply.github.com>
2020-12-31 12:07:32 +0530

    Add files via upload

---
+++ Par4.js
@@ -0,0 +1,41 @@
+
+class At
+{
+	constructor(x,y,r)
+	
+	{
+		var options={
+			isStatic:true,
+			restitution:0.2,
+			friction:0.5,
+			density:1.2
+		
+    }
+
+			
+			
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var pos=this.body.position;		
+
+			push()
+			translate(pos.x, pos.y);
+			fill("blue")
+			circle(this.x,this.y,this.r)
+			
+		
+			
+			pop()
+			
+	}
+
+}
